a29289ef3da73f00ea0b7badc15d7c275b7cdf65
Richard Holland <richard.k.holland@outlook.com>
2020-01-23 13:38:19 -0600

    Module Setup

---
+++ Plaster.ps1
@@ -0,0 +1,22 @@
+$Path = "C:\Git\Inital-Project"
+
+Clear-Host; Push-Location -Path $Path
+
+$destinationPath = New-Item -Path $Path -ItemType Directory -Name InitialModule
+$destinationPath
+
+#$defaultTemplate = Get-PlasterTemplate | Where-Object -FilterScript { $_.Tags -contains 'Module' }
+
+$defaultTemplate = Get-PlasterTemplate | Where-Object -FilterScript { $PSItem.Title -eq 'New PowerShell Manifest Module' }
+$defaultTemplate.TemplatePath
+
+#Invoke Plaster
+
+Invoke-Plaster -TemplatePath $defaultTemplate.TemplatePath -DestinationPath $destinationPath
+
+Import-Module .\InitialModule\Get-Files.psd1
+Get-Module -Name Get-Files
+Get-Command -Module Get-Files
+
+Import-Module -Name Pester
+Invoke-Pester .\InitialModule #Invoke-Pester is run on the directory
