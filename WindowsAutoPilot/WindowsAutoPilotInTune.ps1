ca2ab277805aee34636b9d01f787dcb9ff3437a3
Richard Holland <richard.k.holland@outlook.com>
2020-05-03 13:45:45 -0500

    AutoPilot Script

---
+++ WindowsAutoPilot/WindowsAutoPilotInTune.ps1
@@ -0,0 +1,10 @@
+Find-Module -Name WindowsAutoPilotInTune
+
+Install-module -name WindowsAutoPilotIntune -scope allusers -force
+
+Import-module -name WindowsAutoPilotIntune 
+
+Get-Module -Name WindowsAutoPilotInTune
+
+Get-Command -Name WindowsAutoPilotInTune
+
